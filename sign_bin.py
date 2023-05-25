ecc27a4e673945186c6503e2d0735cae03237891
Ruslan Kuprieiev <kupruser@gmail.com>
2023-05-25 21:59:37 +0300

    sdf

--- sign_bin.py
+++ sign_bin.py
@@ -27,44 +27,50 @@ flags = []
 if args.keychain:
     flags.extend(["--keychain", args.keychain])
 
-for root, _, fnames in os.walk(dvc):
-    for fname in fnames:
-        _, ext = os.path.splitext(fname)
+check_call(
+    [
+        "codesign",
+        "--force",
+        "--verbose",
+        "-s",
+        args.application_id,
+        *flags,
+        "-o",
+        "runtime",
+        "--entitlements",
+        "entitlements.plist",
+        "--timestamp",
+        "--pagesize",
+        "0",
+        "dist/dvc/dvc",
+    ],
+    timeout=300,
+    stderr=STDOUT,
+)
 
-        if ext not in [".so"]:#, ".dylib"]:
-            continue
-
-        fpath = os.path.join(root, fname)
-#        try:
-#            print(f"checking signature on {fpath}")
-#            check_call(
-#                [
-#                    "codesign",
-#                    "--verify",
-#                    fpath,
-#                ],
-#                stderr=STDOUT,
-#            )
-#            print(f"{fpath} is already signed, skipping...")
-#            continue
-#        except CalledProcessError:
-#            pass
+#for root, _, fnames in os.walk(dvc):
+#    for fname in fnames:
+#        _, ext = os.path.splitext(fname)
 #
-        print(f"signing {fpath}")
-        check_call(
-            [
-                "codesign",
-                "--force",
-                "--verbose",
-                "-s",
-                args.application_id,
-                *flags,
-                "-o",
-                "runtime",
-                "--entitlements",
-                "entitlements.plist",
-                fpath,
-            ],
-            timeout=300,
-            stderr=STDOUT,
-        )
+#        if ext not in [".so"]:#, ".dylib"]:
+#            continue
+#
+#        fpath = os.path.join(root, fname)
+#        print(f"signing {fpath}")
+#        check_call(
+#            [
+#                "codesign",
+#                "--force",
+#                "--verbose",
+#                "-s",
+#                args.application_id,
+#                *flags,
+#                "-o",
+#                "runtime",
+#                "--entitlements",
+#                "entitlements.plist",
+#                fpath,
+#            ],
+#            timeout=300,
+#            stderr=STDOUT,
+#        )
