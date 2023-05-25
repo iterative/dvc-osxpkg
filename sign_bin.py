bf54c2f997182f731d5d528090836ac0952bd7a9
Ruslan Kuprieiev <kupruser@gmail.com>
2023-05-25 15:09:06 +0300

    sdf

--- sign_bin.py
+++ sign_bin.py
@@ -18,55 +18,43 @@ args = parser.parse_args()
 path = pathlib.Path(__file__).parent.absolute()
 dvc = path / "dist" / "dvc"
 
-check_call(
-    [
-        "codesign",
-        "--deep",
-        "--force",
-        "--verbose",
-        "-s",
-        args.application_id,
-        "-o",
-        "runtime",
-        "--entitlements",
-        "entitlements.plist",
-        os.fspath(dvc),
-    ],
-    stderr=STDOUT,
-)
 
-#
-#for root, _, fnames in os.walk(dvc):
-#    for fname in fnames:
-#        fpath = os.path.join(root, fname)
-#        try:
-#            print(f"checking signature on {fpath}")
-#            check_call(
-#                [
-#                    "codesign",
-#                    "--verify",
-#                    fpath,
-#                ],
-#                stderr=STDOUT,
-#            )
-#            print(f"{fpath} is already signed, skipping...")
-#            continue
-#        except CalledProcessError:
-#            pass
-#
-#        print(f"signing {fpath}")
-#        check_call(
-#            [
-#                "codesign",
-#                "--force",
-#                "--verbose",
-#                "-s",
-#                args.application_id,
-#                "-o",
-#                "runtime",
-#                "--entitlements",
-#                "entitlements.plist",
-#                fpath,
-#            ],
-#            stderr=STDOUT,
-#        )
+for root, _, fnames in os.walk(dvc):
+    for fname in fnames:
+        _, ext = os.path.splitext(fname)
+
+        if ext not in [".so", ".dylib"]:
+            continue
+
+        fpath = os.path.join(root, fname)
+        try:
+            print(f"checking signature on {fpath}")
+            check_call(
+                [
+                    "codesign",
+                    "--verify",
+                    fpath,
+                ],
+                stderr=STDOUT,
+            )
+            print(f"{fpath} is already signed, skipping...")
+            continue
+        except CalledProcessError:
+            pass
+
+        print(f"signing {fpath}")
+        check_call(
+            [
+                "codesign",
+                "--force",
+                "--verbose",
+                "-s",
+                args.application_id,
+                "-o",
+                "runtime",
+                "--entitlements",
+                "entitlements.plist",
+                fpath,
+            ],
+            stderr=STDOUT,
+        )
